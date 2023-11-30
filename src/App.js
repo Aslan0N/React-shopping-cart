434cc62dc8e7a72a7125b857b0a4a9422202a5af
Aslan0N <thenzrv@gmail.com>
2023-11-30 12:02:04 +0400

    todo-list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// React
+import React from 'react'
+
+// Components
+import TodoForm from './Components/TodoForm'
+
+const App = () => {
+  return (
+      <TodoForm/>
+  )
+}
+
+export default App
